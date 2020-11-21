fc35729a5141be4ee286b1bbcd7180b15b38f4a4
suive <is@suive.co>
2020-11-21 22:39:50 +0300

    Use relative paths

--- lib/readlist.el
+++ lib/readlist.el
@@ -4,7 +4,7 @@
 ;;; Code:
 
 (define-minor-mode readlist-mode
-  " Toggle Readlist mode"
+  "Toggle Readlist mode"
   :init-value nil
   :lighter " Readlist"
   :keymap (make-sparse-keymap))
@@ -13,23 +13,23 @@
   "Move the selected book to the ACTIVE status and create a notes org file for it."
   (interactive)
   (org-todo "ACTIVE")
-  (let ((element (org-element-at-point))
-        (current-project (projectile-project-root)))
-    (let ((book-name (org-element-property :raw-value element)))
-      (let ((new-file (concat current-project "notes/books/" book-name ".org")))
-        ;; Create a new notes file.
-        (shell-command (concat "touch '" new-file "'"))
+  (let* ((element (org-element-at-point))
+         (current-project (projectile-project-root))
+         (book-name (org-element-property :raw-value element))
+         (new-file (concat "notes/books/" book-name ".org"))
+         (new-file-absolute (concat current-project new-file)))
+    ;; Create a new notes file.
+    (shell-command (concat "touch '" new-file-absolute "'"))
 
-        ;; Put the newly created file to the project cache.
-        (unless (projectile-file-cached-p new-file current-project)
-          (puthash current-project
-                   (cons new-file (gethash current-project projectile-projects-cache))
-                   projectile-projects-cache)
-          (projectile-serialize-cache))
+    ;; Put the newly created file to the project cache.
+    (unless (projectile-file-cached-p new-file-absolute current-project)
+      (puthash current-project
+               (cons new-file-absolute (gethash current-project projectile-projects-cache))
+               projectile-projects-cache)
+      (projectile-serialize-cache))
 
-        ;; Replace the heading with the link to the file.
-        (org-edit-headline (concat "[[file:" new-file "][" book-name "]]"))
-  ))))
+    ;; Replace the heading with the link to the file.
+    (org-edit-headline (concat "[[file:" new-file "][" book-name "]]"))))
 
 (provide 'readlist)
 
